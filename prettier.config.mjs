1c944e0c2b9adad1bc84bc9d33091e4602a0bb17
Zach Van Handel <zach@zachsvanhandel.me>
2026-04-01 22:06:39 -0600

    ci: set up prettier (#2)

---
+++ prettier.config.mjs
@@ -0,0 +1,7 @@
+const config = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindFunctions: ["clsx", "cva", "cn"],
+  tailwindStylesheet: "./src/app/globals.css",
+};
+
+export default config;
